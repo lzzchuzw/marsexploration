7f0766ce4fbb9df8a06f3c2796ca0a87bba9082c
lzzchuzw <297081999@qq.com>
2021-02-23 09:02:19 +0800

    Initial commit

---
+++ src/main/java/com/exploration/marsexploration/MarsexplorationApplication.java
@@ -0,0 +1,13 @@
+package com.exploration.marsexploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsexplorationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsexplorationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exploration/marsexploration/MarsexplorationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploration.marsexploration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsexplorationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
